3daf976e524a39d0c877c922e67ad5aea26b2092
netcool <51483807+toksichny@users.noreply.github.com>
2022-07-19 17:01:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
-@app.route('/buy')
-def about():
-    return render_template('index.html')
+#@app.route('/buy')
+#def about():
+    #return render_template('index.html')
 
 
 if __name__ == "__main__":
